6f65b9cfd8fcaa3a0d239e04f8ff9cf3a1815b96
Алексей Селезнев <a.seleznev@g.nsu.ru>
2025-08-17 14:07:49 +0700

    Правки: Цены + Фотографии

--- src/components/Pictures/Pictures.js
+++ src/components/Pictures/Pictures.js
@@ -13,6 +13,19 @@ import danariPng8 from "../../assets/img/danari8.png";
 import danariPng9 from "../../assets/img/danari9.png";
 import danariPng10 from "../../assets/img/danari10.png";
 import danariPng11 from "../../assets/img/danari11.png";
+import danariPng17 from "../../assets/img/danari17.png";
+import danariPng18 from "../../assets/img/danari18.png";
+import danariPng19 from "../../assets/img/danari19.png";
+import danariPng20 from "../../assets/img/danari20.png";
+import danariPng21 from "../../assets/img/danari21.png";
+import danariPng22 from "../../assets/img/danari22.png";
+import danariPng23 from "../../assets/img/danari23.png";
+import danariPng24 from "../../assets/img/danari24.png";
+import danariPng25 from "../../assets/img/danari25.png";
+import danariPng26 from "../../assets/img/danari26.png";
+import danariPng27 from "../../assets/img/danari27.png";
+import danariPng28 from "../../assets/img/danari28.png";
+import danariPng29 from "../../assets/img/danari29.png";
 
 const Pictures = () => {
     return (
@@ -70,6 +83,45 @@ const Pictures = () => {
                 </SwiperSlide>
                 <SwiperSlide>
                     <img src={danariPng7} alt="DanariDance"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={danariPng17} alt="DanariDance"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={danariPng18} alt="DanariDance"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={danariPng19} alt="DanariDance"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={danariPng20} alt="DanariDance"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={danariPng21} alt="DanariDance"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={danariPng22} alt="DanariDance"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={danariPng23} alt="DanariDance"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={danariPng24} alt="DanariDance"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={danariPng25} alt="DanariDance"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={danariPng26} alt="DanariDance"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={danariPng27} alt="DanariDance"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={danariPng28} alt="DanariDance"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={danariPng29} alt="DanariDance"/>
                 </SwiperSlide>
             </Swiper>
         </Container>
